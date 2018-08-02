8176323abafe4880b0587169cde24451475b0cf0
Simon Cass <scass1991@gmail.com>
2018-08-02 22:26:45 +0100

    initial commit 8 kyu

---
+++ oppositenumber.rb
@@ -0,0 +1,17 @@
+=begin
+August 2nd 2018
+8 Kyu
+Very simple, given a number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?
+https://www.codewars.com/kata/opposite-number/train/ruby
+=end
+
+def opposite(n)
+    puts 0-n
+end
+
+opposite(3)
