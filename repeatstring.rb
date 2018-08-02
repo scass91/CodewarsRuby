6d441ceadf57c23582c8fb4582f08dbcce4eeb0e
Simon Cass <scass1991@gmail.com>
2018-08-02 22:18:50 +0100

    initial commit - 8kyu

---
+++ repeatstring.rb
@@ -0,0 +1,10 @@
+=begin
+August 2nd 2018
+Repeat a string a given number of times
+8 KYU
+https://www.codewars.com/kata/string-repeat/train/ruby
+=end
+def repeat_str (n, s)
+s*n
+end
+repeat_str (5, "Hello")
