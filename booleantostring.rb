0922bccbdd983f65012631ae12e01cba5d89ac87
Simon Cass <scass1991@gmail.com>
2018-08-04 20:14:21 +0100

    Initial commit

---
+++ booleantostring.rb
@@ -0,0 +1,11 @@
+=begin
+August 4th 2018
+8 KYU
+https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no/train/ruby
+Complete the method that takes a boolean value and return a "Yes" string for
+true, or a "No" string for false.
+=end
+
+def bool_to_word bool
+  bool ? "Yes" : "No"
+end
