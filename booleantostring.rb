c1b332c751190b88c11681765dadd565150521bf
Simon Cass <scass1991@gmail.com>
2018-08-05 13:09:33 +0100

    8KYU

--- booleantostring.rb
+++ booleantostring.rb
@@ -4,6 +4,7 @@ August 4th 2018
 https://www.codewars.com/kata/convert-boolean-values-to-strings-yes-or-no/train/ruby
 Complete the method that takes a boolean value and return a "Yes" string for
 true, or a "No" string for false.
+
 =end
 
 def bool_to_word bool
--- returnnegative.rb
+++ returnnegative.rb
@@ -16,6 +16,7 @@ Notes:
 The number can be negative already, in which case no change is required.
 Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 =end
+
 def makeNegative(num)
   num > 0 ? -num : num
 end
