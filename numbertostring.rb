d6b3f65ec7378cbb114c116512be976e3d230c3b
Simon Cass <scass1991@gmail.com>
2018-08-05 12:40:07 +0100

    initial commit

---
+++ numbertostring.rb
@@ -0,0 +1,17 @@
+=begin
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+
+number-to-string 123 ;; returns '123'
+number-to-string 999 ;; returns '999'
+August 5th 2018
+8 KYU
+https://www.codewars.com/kata/convert-a-number-to-a-string/train/ruby
+=end
+
+def numberToString(num)
+  return num.to_s
+end
