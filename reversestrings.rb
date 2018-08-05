ef799ba9f64d7125e819956a6ffd45a011f25db9
Simon Cass <scass1991@gmail.com>
2018-08-05 15:17:05 +0100

    Initial commit 8 kyu

---
+++ reversestrings.rb
@@ -0,0 +1,16 @@
+=begin
+August 5th 2018
+Reverse string
+
+Complete the solution so that it reverses the string value passed into it.
+
+solution('world') # returns 'dlrow'
+
+8KYU
+https://www.codewars.com/kata/reversed-strings/train/ruby
+
+=end
+
+def solution(str)
+  str.reverse
+end
