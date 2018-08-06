ecc7aa7dc03cab408ceb5fdecb44d3ddb7d0807c
Simon Cass <scass1991@gmail.com>
2018-08-06 23:30:55 +0100

    initial commit 8 kyu

---
+++ strtonumbeer.rb
@@ -0,0 +1,28 @@
+=begin
+August 6th 2018
+Convert string to number
+Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+Description
+
+We need a function that can transform a string into a number. What
+ways of achieving this do you know?
+
+Note: Don't worry, all inputs will be strings, and every string is a perfectly
+ valid representation of an integral number.
+
+Examples
+
+string_to_number "1234"  == 1234
+string_to_number "605"   == 605
+string_to_number "1405"  == 1405
+string_to_number "-7"    == -7
+
+8KYU
+https://www.codewars.com/kata/convert-a-string-to-a-number/train/ruby
+=end
+
+
+def string_to_number(s)
+ s.to_i
+end
