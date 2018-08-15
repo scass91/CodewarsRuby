ce934c0dce1fade72b19242a2bc88020cfce5b70
Simon Cass <scass1991@gmail.com>
2018-08-15 21:21:34 +0100

    initial commit 8 kyu

---
+++ listminandmax.rb
@@ -0,0 +1,13 @@
+=begin
+August 15th 2018
+Return minimum & maximum from a list8 KYU
+https://www.codewars.com/kata/find-maximum-and-minimum-values-of-a-list/train/ruby
+=end
+
+def min(list)
+ list.min
+end
+
+def max(list)
+  list.max
+end
