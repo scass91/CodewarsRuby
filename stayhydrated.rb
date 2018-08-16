e556b3d3d311dd54319b4b82cf46f03aae21fe1b
Simon Cass <scass1991@gmail.com>
2018-08-16 22:10:46 +0100

    initial commit 8 kyu

---
+++ stayhydrated.rb
@@ -0,0 +1,16 @@
+=begin
+August 16th 2018
+Stay hydrated!
+8 KYU
+https://www.codewars.com/kata/invert-values/train/ruby
+=end
+
+def litres(time)
+  litres = 0.5*time.to_f
+  litres.to_i
+end
+
+#better solution
+#def litres(time)
+#  (time * 0.5).floor
+#end
