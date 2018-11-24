441e6e3896e7d5b576655a20f2b8e2cb71d2c5a2
Simon Cass <scass1991@gmail.com>
2018-11-24 23:58:51 +0000

    Initial commit - 7 KYU

---
+++ NoOdditiesHere.rb
@@ -0,0 +1,7 @@
+# Write a small function that returns the values of an array that are not odd.
+
+# All values in the array will be integers. Return the good values in the order they are given.
+
+def no_odds( values )
+values.select { |i| i.even? }
+end
